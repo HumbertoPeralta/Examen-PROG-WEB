0393f11565d50815d1a7ea403ccb634bd449d1bf
HumbertoPeralta <hperalta_22@alu.uabcs.mx>
2024-12-04 01:41:26 -0700

    Controller agregados

---
+++ proyecto/src/controllers/LogoutController.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__.'/../helpers/functions.php';
+
+session_start();
+
+session_destroy();
+
+header('Location: '.BASE_URL.'/login');
+
+exit;
+
+?>
---
+++ proyecto/src/helpers/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+if (session_status() === PHP_SESSION_NONE) {
+
+    session_start();
+
+}
+
+
+
+if (!isset($_SESSION['user'])) {
+
+    header('Location: '.BASE_URL.'/login');
+
+    exit;
+
+}
