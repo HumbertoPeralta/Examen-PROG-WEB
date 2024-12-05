142edc1878c418ce64ad6997c83ae8fc080f9dec
HumbertoPeralta <hperalta_22@alu.uabcs.mx>
2024-12-05 00:47:24 -0700

    redirecciones funcionando

--- proyecto/public/index.php
+++ proyecto/public/index.php
@@ -10,7 +10,7 @@ $request = $_SERVER['REQUEST_URI'];
 $request = strtok($request, '?');
 
 switch($request){
-    case '/':
+    case URL . '/':
         require_once __DIR__.'/../src/views/public/welcome.php';
         break;
     case '/login':
@@ -20,7 +20,7 @@ switch($request){
         require_once __DIR__.'/../src/controllers/LogoutController.php';
         break;
     default:
-        require_once __DIR__.'/../src/views/public/welcome.php';
-        break;
+        http_response_code(404);        
+    break;
 
 }
---
+++ proyecto/src/views/public/tshirts/playeras.php
@@ -0,0 +1,21 @@
+<?php 
+include_once  __DIR__ .'/../layouts/header.php';
+?>
+
+<?php
+
+$tshirts = getTshirts();
+
+if (count($tshirts) > 0) {
+    foreach ($tshirts as $tshirt) {
+        echo "<h3>" .($tshirt['nombre']) . "</h3>";
+        echo "<p>Precio: $" . number_format($tshirt['precio'], 2) . "</p>";
+    }
+} else {
+    echo "<p>No hay productos disponibles.</p>";
+}
+?>
+
+<?php
+include_once  __DIR__ .'/../layouts/footer.php';
+?>
