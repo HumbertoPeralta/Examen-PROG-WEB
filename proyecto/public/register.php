3a5d88618eb9be58dc5c3cbbf9f9c2c3156b65d6
Adan <joge_22@alu.uabcs.mx>
2024-12-05 19:30:22 -0700

    Crear registro

---
+++ proyecto/public/register.php
@@ -0,0 +1,72 @@
+<?php
+require __DIR__ . '/../src/helpers/functions.php';
+require_once __DIR__ . '/../src/controllers/LoginController.php';
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="<?= ASSETS_URL ?>/css/styleLogin.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="container">
+        <a href="<?= BASE_URL ?>/">
+            <img src="<?= ASSETS_URL ?>/img/logo.svg" alt="Imagen" class="small-img">
+        </a>
+        <h2>Iniciar sesión</h2>
+
+        <?php
+
+        if (isset($_SESSION['errors'][0]))
+
+            echo "<p style='color:red;'>" . $_SESSION['errors'][0] . "</p>";
+
+        unset($_SESSION['errors']);
+
+        ?>
+
+        <form method="POST" action="<?= SRC_URL ?>/controllers/LoginController.php">
+
+            <div class="input-group">
+                <label for="username">Nombre</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div class="input-group">
+                <label for="username">Apellido</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div class="input-group">
+                <label for="username">Correo electronico</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div class="input-group">
+                <label for="password">Contraseña</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <div class="input-group">
+                <label for="password">Confirmar Contraseña</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+
+            <button type="submit">Entrar</button>
+            <div class="extra-options">
+                <a href="#">¿Olvidaste tu contraseña?</a>
+                <a href="#">Crear cuenta</a>
+            </div>
+        </form>
+
+    </div>
+
+</body>
+
+</html>
--- proyecto/src/views/public/tshirts/playeras.php
+++ proyecto/src/views/public/tshirts/playeras.php
@@ -9,23 +9,24 @@ include_once  __DIR__ .'../../../layouts/header.php';
 
 <?php
 
+
 $tshirts = getTshirts();
 
 if (count($tshirts) > 0) {
-    echo '<div class="card-container">';
     foreach ($tshirts as $tshirt) {
-        echo '<div class="card">';
-        echo '<img src="' . htmlspecialchars($tshirt['imagen']) . '" alt="' . htmlspecialchars($tshirt['nombre']) . '">';
-        echo '<h3>' . htmlspecialchars($tshirt['nombre']) . '</h3>';
-        /* echo '<p>' . htmlspecialchars($tshirt['descripcion']) . '</p>'; */
-        echo '<p>Precio: $' . number_format($tshirt['precio'], 2) . '</p>';
-        echo '</div>';
+        ?>
+
+<img src="<?=ASSETS_URL?>/img/<?php echo htmlspecialchars($tshirt['imagen']) ?>"alt="...">
+
+        <h3><?php echo htmlspecialchars($tshirt['nombre'])?>  </h3>;
+       <p> <?php echo htmlspecialchars($tshirt['descripcion'])?> </p>; 
+        <p> <?php echo "Precio: $". number_format($tshirt['precio'], 2)?>  </p>;
+<?php
     }
 } else {
     echo "<p>No hay productos disponibles.</p>";
 }
 ?>
-</div>
 
 <?php
 include_once  __DIR__ .'../../../layouts/footer.php';
