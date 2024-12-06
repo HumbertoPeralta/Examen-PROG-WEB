bc091848a90c10f0fdd9114fac51981c4fb42c6e
Adan <joge_22@alu.uabcs.mx>
2024-12-06 01:39:07 -0700

    modificaciones

--- proyecto/src/views/public/tshirts/playeras.php
+++ proyecto/src/views/public/tshirts/playeras.php
@@ -1,7 +1,7 @@
 <?php 
 include_once __DIR__ . '../../../layouts/header.php';
 ?>
-<link rel="stylesheet" href="<?= ASSETS_URL ?>/css/style.css">
+<link rel="stylesheet" href="<?= ASSETS_URL ?>/css/styleProducto.css">
 
 <div class="title-container">
     <h1 class="centered-title">Playeras</h1>
